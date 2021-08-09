fa9d7c6046e585c600f89286bcbcda0530beb431
blackhole20 <81379313+blackhole20@users.noreply.github.com>
2021-08-09 15:02:38 -0600

    Add files via upload

--- SCA_room.js
+++ SCA_room.js
@@ -13,3 +13,38 @@ var firebaseConfig = {
 var user_name=localStorage.getItem("name");
 
 document.getElementById("username").innerHTML= "welcome "+ user_name;
+
+function addRoom(){
+  room_name = document.getElementById("addRoom").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+    
+    window.location = "SCA_page.html";
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+      //End code
+      });});}
+getData();
+
+function redirectToRoomName(name)
+{
+  console.log(name);
+  localStorage.setItem("room_name", name);
+    window.location = "SCA_page.html";
+}
+
+function logOut() {
+localStorage.removeItem("name");
+localStorage.removeItem("room_name");
+    window.location = "index.html";
+}
